cf5d5dc8ed125aa7928c74086d439eca89b555c8
Kenlove Chereste <cherestek@gmail.com>
2022-04-28 23:26:26 -0300

    function Idade  js

---
+++ sintax/funcção.js
@@ -0,0 +1,10 @@
+    function comparanumeros(num1, num2){
+        const saoiguais = num1 === num2;
+        const soma = num1 + num2;
+
+        if(saoiguais){
+            return "saoiguais";
+        }
+
+        return " Nao sao iguais";
+    } 
